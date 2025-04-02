52b842ecdfb948916c22369ab244b5b16d6cf0b2
RichardPinheiro <richardpinheiro1992@gmail.com>
2025-04-01 22:01:40 -0300

    Ducumenting emitter functions

--- src/emitter.c
+++ src/emitter.c
@@ -1,25 +1,57 @@
 #include "game.h"
 
+/**
+ * @brief Emits an event indicating a change in life status between two sprites.
+ *
+ * This function creates and emits a game event that signifies a change in the life
+ * status, involving a source and a target sprite.
+ *
+ * @param source A pointer to the source Sprite involved in the life change event.
+ * @param target A pointer to the target Sprite involved in the life change event.
+ * @return EmitterResult indicating the success of the event emission.
+ */
 EmitterResult emit_life_change(Sprite* source, Sprite* target) {
     GameEvent life_event = {
         .type = EVENT_LIFE_CHANGED,
-        .payload.collision.source = source,
-        .payload.collision.target = target
+        .payload.collision = {
+            .source = source,
+            .target = target
+        }
     };
     emit_event(life_event);
     return EMITTER_SUCCESS;
 }
 
+/**
+ * @brief Emits an event indicating a change in rings status between two sprites.
+ *
+ * This function creates and emits a game event that signifies a change in the rings
+ * status, involving a source and a target sprite.
+ *
+ * @param source A pointer to the source Sprite involved in the rings change event.
+ * @param target A pointer to the target Sprite involved in the rings change event.
+ * @return EmitterResult indicating the success of the event emission.
+ */
 EmitterResult emit_rings_change(Sprite* source, Sprite* target) {
     GameEvent rings_event = {
         .type = EVENT_RINGS_CHANGED,
-        .payload.collision.source = source,
-        .payload.collision.target = target
+        .payload.collision = {
+            .source = source,
+            .target = target
+        }
     };
     emit_event(rings_event);
     return EMITTER_SUCCESS;
 }
 
+/**
+ * @brief Emits a sound effect event.
+ *
+ * This function creates and emits a game event to play a sound effect, based on the provided AudioID.
+ *
+ * @param sfx_id The identifier for the sound effect to be played.
+ * @return EmitterResult indicating the success of the event emission.
+ */
 EmitterResult emit_sfx(AudioID sfx_id) {
     GameEvent sfx_event = {
         .type = EVENT_SOUND_EFFECT,
@@ -29,6 +61,16 @@ EmitterResult emit_sfx(AudioID sfx_id) {
     return EMITTER_SUCCESS;
 }
 
+/**
+ * @brief Emits a music play event.
+ *
+ * This function creates and emits a game event to play background music. It takes an AudioID and a loop flag,
+ * indicating whether the music should be looped.
+ *
+ * @param music_id The identifier for the music track to be played.
+ * @param loop A boolean flag that indicates if the music should loop continuously.
+ * @return EmitterResult indicating the success of the event emission.
+ */
 EmitterResult emit_music(AudioID music_id, bool loop) {
     GameEvent music_event = {
         .type = EVENT_MUSIC_PLAY,
@@ -41,6 +83,13 @@ EmitterResult emit_music(AudioID music_id, bool loop) {
     return EMITTER_SUCCESS;
 }
 
+/**
+ * @brief Emits a game over event.
+ *
+ * This function creates and emits a game event indicating that the game is over.
+ *
+ * @return EmitterResult indicating the success of the event emission.
+ */
 EmitterResult emit_game_over(void) {
     GameEvent game_over_event = {
         .type = EVENT_GAME_OVER
@@ -49,6 +98,13 @@ EmitterResult emit_game_over(void) {
     return EMITTER_SUCCESS;
 }
 
+/**
+ * @brief Queues a game event.
+ *
+ * This function takes a GameEvent and enqueues it into the global event queue.
+ *
+ * @param event The GameEvent to be queued.
+ */
 void emit_event(GameEvent event) {
     queue_event(&global_queue, event);
 }
